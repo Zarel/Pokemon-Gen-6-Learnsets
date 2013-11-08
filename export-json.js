60d086412fe8410090ea227e4860d1b91e69a830
Guangcong Luo <guangcongluo@gmail.com>
2013-11-08 02:31:22 -0600

    JSON exporter now preserves same-level learning order

--- export-json.js
+++ export-json.js
@@ -31,7 +31,7 @@ for (var i=0; i<input.length; i++) {
 	if (!line) continue;
 	if (line.charAt(0) === '(') continue; // this is a comment
 	if (line.charAt(0) === '=') {
-		speciesid = toId(line);
+		speciesid = line.substr(3, line.length-6);
 		species = line;
 		continue;
 	}
@@ -40,30 +40,29 @@ for (var i=0; i<input.length; i++) {
 		console.error("Line '"+line+"' must be in the format 'Source - Move' for "+species);
 		continue;
 	}
-	var moveid = toId(parts[1]);
+	var moveid = parts[1];
 	var typeString = parts[0].toLowerCase().replace(/ /g,'').replace('.','');
 
 	var type = '';
 	var level = '';
-	if (typeString.substr(0,2) in {tm:1,hm:1}) type = '6M';
-	else if (typeString === 'tutor') type = '6T';
-	else if (typeString === 'egg') type = '6E';
-	else if (typeString === 'moverelearner' || typeString === 'relearn' || typeString === 'mr' || typeString === 'l?' || typeString === '?') type = '6L0';
-	else if (typeString === 'start') type = '6L1';
+	if (typeString.substr(0,2) in {tm:1,hm:1}) type = 'M';
+	else if (typeString === 'tutor') type = 'T';
+	else if (typeString === 'egg') type = 'E';
+	else if (typeString === 'moverelearner' || typeString === 'relearn' || typeString === 'mr' || typeString === 'l?' || typeString === '?') type = '?';
+	else if (typeString === 'start') type = 'L1';
 	else if (typeString.substr(0,1) === 'l') {
 		level = typeString.substr(1);
 	}
 	if (!type && !level) level = typeString;
 	if (level) {
-		if (level.charAt(0) in {'<':1,'>':1,'?':1}) type = '6L0';
-		else if (/^[0-9]{1,3}$/.test(level)) type = '6L'+(parseInt(level, 10)||1);
+		if (level.charAt(0) in {'<':1,'>':1,'?':1}) type = '?';
+		else if (/^[0-9]{1,3}$/.test(level)) type = 'L'+(parseInt(level, 10)||1);
 	}
 	if (!type) console.error("Source '"+parts[0]+"' in '"+line+"' not formatted correctly for "+species);
 	if (type) {
-		if (!LearnsetsG6[speciesid]) LearnsetsG6[speciesid] = {learnset:{}};
+		if (!LearnsetsG6[speciesid]) LearnsetsG6[speciesid] = [];
 
-		if (!LearnsetsG6[speciesid].learnset[moveid]) LearnsetsG6[speciesid].learnset[moveid] = [type];
-		else LearnsetsG6[speciesid].learnset[moveid].push(type);
+		LearnsetsG6[speciesid].push([type, moveid]);
 	}
 }
 
